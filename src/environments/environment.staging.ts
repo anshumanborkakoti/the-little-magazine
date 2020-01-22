b294a09250957b069c77ce7f5d7d6f6bfc2f1b7c
Anshuman Borkakoti <anshumanborkakoti@gmail.com>
2020-01-22 20:59:36 +0530

    Removed environments

--- src/environments/environment.staging.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  cloudinary_cloud_name: 'the-little-journal-com',
-  cloudinary_deliveryUrl_prefix: 'tljne',
-  api_url: 'http://tljne-env-staging.ap-south-1.elasticbeanstalk.com/tljneapi'
-};
